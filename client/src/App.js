6ecfb114e90c6eef16efa8a4d4328c284223e909
1sfaccia1 <chad.m.wittman75@gmail.com>
2019-09-24 04:22:02 -0500

    goodnight

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const topics = [
+  {
+    name: "React Router",
+    id: "react-router",
+    description: "Declarative, component based routing",
+    resources: [{
+      name: "URL Parameters",
+      id: "url-parameters",
+      description: "Url parameters are parameters which",
+      url: "https://www.youtube.com/watch?v=sfvrjwVihFY" 
+    }, {
+      name: "Programmatically navigate",
+        id: "programmatically-navigate",
+        description: "When building an app with React",
+        url: "https://reacttraining.com/react-router/web/example/no-match" }]
+  },
+  {
+    name: "React.js",
+      id: "reactjs",
+    description: "A JavaScript library for building user",
+    resources: [{
+      name: "React Lifecycle Events",
+      id: "react-lifecycle",
+      description: "React Lifecycle events allow you to tie into specific events",
+      url: "https://www.youtube.com/watch?v=sfvrjwVihFY"
+    }, {
+      name: "React AHA Moments",
+        id: "react-aha",
+        description: "A collection of 'Aha moments while learning React.'",
+        url: "https://www.youtube.com/watch?v=sfvrjwVihFY" }]
+  },
+  {
+    name: "Functional Programing",
+    id: "functional-programing",
+    description: "In computer science, functional programing is a program",
+    resources: [{
+      name: "imperative vs Declarative programming",
+      id: "imperative-declarative",
+      description: "A guide to underStanding the difference between",
+      url: ""
+    }, {
+      name: "Building User Interfaces with Pure Functional programming",
+        id: "fn-compostion",
+        description: "A guild to builing ui with pur functions",
+        url: "https://www.youtube.com/watch?v=sfvrjwVihFY" }]
+  }
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
     </div>
   );
 }
